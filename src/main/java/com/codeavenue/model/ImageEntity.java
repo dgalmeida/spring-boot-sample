6000ce573497908c4a1a52e39f56dfeb64a787a3
Diego Almeida <dgralmeida@gmail.com>
2016-09-24 00:41:03 -0300

    inserting all spring dependencies

---
+++ src/main/java/com/codeavenue/model/ImageEntity.java
@@ -0,0 +1,10 @@
+package com.codeavenue.model;
+
+/**
+ * #description#
+ *
+ * @author <a href="mailto:diegogr@ciandt.com">Diego G. R. Almeida</a>
+ * @since 9/23/16
+ */
+public class ImageEntity {
+}
---
+++ src/main/java/com/codeavenue/persistence/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.codeavenue.persistence;
+
+/**
+ * #description#
+ *
+ * @author <a href="mailto:diegogr@ciandt.com">Diego G. R. Almeida</a>
+ * @since 9/23/16
+ */
+public class ProductRepository {
+}
