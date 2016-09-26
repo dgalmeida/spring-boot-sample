59aa51a4b838bd6e6efd7ba65d7c92fc4d7587ea
Diego Almeida <dgralmeida@gmail.com>
2016-09-26 15:05:27 -0300

    readme file - swagger config - uniq place to named queries

---
+++ src/main/java/com/codeavenue/SwaggerConfig.java
@@ -0,0 +1,10 @@
+package com.codeavenue;
+
+/**
+ * #description#
+ *
+ * @author <a href="mailto:diegogr@ciandt.com">Diego G. R. Almeida</a>
+ * @since 9/26/16
+ */
+public class SwaggerConfig {
+}
---
+++ src/main/java/com/codeavenue/persistence/CustomNamedQueries.java
@@ -0,0 +1,31 @@
+package com.codeavenue.persistence;
+
+/**
+ * #description#
+ *
+ * @author <a href="mailto:diegogr@ciandt.com">Diego G. R. Almeida</a>
+ * @since 9/26/16
+ */
+public final class CustomNamedQueries {
+
+  public static final String FIND_ALL_PRODUCT_ONLY_CUSTOM_NAME =
+      "ProductEntity.findAllProductsOnly";
+
+  public static final String FIND_ALL_PRODUCT_ONLY_CUSTOM_QUERY =
+      "SELECT new " +
+          "com.codeavenue.model.dtos.ProductOnlyDto(p.productId, p.name, p.description) " +
+          "FROM ProductEntity p";
+
+  public static final String FIND_ONE_PRODUCT_ONLY_CUSTOM_NAME =
+      "ProductEntity.findProductOnly";
+
+  public static final String FIND_ONE_PRODUCT_ONLY_CUSTOM_QUERY =
+      "SELECT new " +
+          "com.codeavenue.model.dtos.ProductOnlyDto(p.productId, p.name, p.description) " +
+          "FROM ProductEntity p WHERE p.productId = ?1";
+
+  public static final String FIND_CHILD_PRODUCTS_NAME = "ProductEntity.findChildProducts";
+
+  public static final String FIND_CHILD_PRODUCTS_QUERY =
+      "SELECT p from ProductEntity p WHERE p.parentProduct.id = ?1";
+}
